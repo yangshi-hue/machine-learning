56fd902cec4d73abf662d244ce9783e4c5dc9e5d
yangshi <yangshinembang@gmail.com>
2026-09-24 09:29:08 +0900

    visualizing the dataset

--- customer_segmentation.py
+++ customer_segmentation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 data = pd.read_csv("customer_data.csv")
 X = data[["annual_income","spending_score"]]
 #clustering the data set
@@ -9,3 +10,14 @@ model.fit(X)
 #get the clusters label
 labels = model.labels_
 print(labels)
+#visualizing
+plt.scatter(
+    X["annual_income"],
+    X["spending_score"],
+    c = labels
+)
+plt.xlabel("annual income")
+plt.ylabel("spending score")
+plt.title("customer segmentation")
+
+plt.show()
